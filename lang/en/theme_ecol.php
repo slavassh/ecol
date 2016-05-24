6c7832cb2ac8840c7a6a1d15001aab86af4879dd
Viacheslav Shestakov <slavassh@gmail.com>
2016-05-24 11:09:52 +0500

    Первый коммит

---
+++ lang/en/theme_ecol.php
@@ -0,0 +1,28 @@
+﻿<?php
+
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Strings for component 'theme_ecol', language 'en', branch 'MOODLE_20_STABLE'
+ *
+ * @package   theme_ecol
+ * @copyright 2011 Vyacheslav Shestakov 
+ */
+
+$string['pluginname'] = 'Ecol';
+$string['region-side-post'] = 'Right';
+$string['region-side-pre'] = 'Left';
+$string['choosereadme'] = '<p align="right">Theme created by order of the Chelyabinsk Economic College.<br/>Shestakov Vyacheslav<br/>e-mail: slavash@ecol.edu.ru<br/>19.09.2011</p>';
